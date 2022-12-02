b1ace513f1b0476cf85cd79d2deb52d09383f487
Maria <maricruzvelazquezmartinez01@gmail.com>
2022-12-02 11:29:06 -0300

    Libreria-Ionic(html)

--- src/app/sesion.guard.ts
+++ src/app/sesion.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { SesionService } from './servicios/sesion.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
